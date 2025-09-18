5bb5d7f3bc4d2dd47d2fa8f7f567e95f4ce84454
NMoreira25 <nathalia.moreira74@icloud.com>
2025-09-18 11:38:59 -0300

    if,elif,else.py

---
+++ Modulo 1/Condicionais/if,elif,else.py
@@ -0,0 +1,14 @@
+
+nome_cadastro = input("digite seu nome de cadastro: ")
+senha_cadastro = input("digite sua senha de cadastro: ")
+opçao = 7
+while opçao !=3:  
+
+    nome_digitado = input("digite seu nome digitado: ")
+    senha_digitada = input("digite sua senha de cadastro: ")
+
+    if nome_cadastro == nome_digitado and senha_cadastro == senha_digitada:
+        print("correto")
+    else: 
+        print("errado") 
+    opçao  = int(input("digite 2 para tentar novamente /n 3 para sair"))
