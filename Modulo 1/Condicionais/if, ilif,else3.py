6624bd13b94d0ca213c390afa61ce09affc8c830
NMoreira25 <nathalia.moreira74@icloud.com>
2025-09-18 11:41:14 -0300

    if, ilif,else3.py

---
+++ Modulo 1/Condicionais/if, ilif,else3.py
@@ -0,0 +1,9 @@
+
+clima = float(input("digite como esta o clima: "))
+
+if clima<10.0:
+    print("esta frio hoje")
+elif clima<19.5:
+    print("esta meio termo hoje")
+else:
+    print("esta quente hoje")  
