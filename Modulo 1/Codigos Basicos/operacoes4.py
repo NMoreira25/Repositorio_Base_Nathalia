660f9b025b50c7e61fe77968c56078883019ca7b
NMoreira25 <nathalia.moreira74@icloud.com>
2025-09-18 11:36:15 -0300

    opperacoes4.py

---
+++ Modulo 1/Codigos Basicos/operacoes4.py
@@ -0,0 +1,5 @@
+nome_do_filme = input("digite o nome do filme: ")valor_do_filme = float(input("digite o valor do filme: "))consumos_do_filme = float(input("digite o consumo do filme: "))print("----------------------------------")print("| filme:" , nome_do_filme)print("| valor:", valor_do_filme)print("| consumo do filme:", consumos_do_filme) 
+Collapse file: moreira.py‎moreira.py
+nome = input("qual é o seu nome? ")idade = input("qual a sua idade? ")print("nome da pessoa: " , nome)print("idade da pessoa: " , idade)
+Collapse file: nathy.py‎nathy.py‎
+nome = "Nathalia"idade = 15 print("o nome dela é:" , nome)print("a idade dela é:" , idade)
